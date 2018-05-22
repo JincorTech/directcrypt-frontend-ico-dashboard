f3c46954f7df03449f263a7e54f859287f6d01ce
David Rahamin <34671819+drahamin@users.noreply.github.com>
2018-05-22 11:33:03 +0200

    update to index.js
    language update

--- src/components/verification/Success/index.js
+++ src/components/verification/Success/index.js
@@ -5,8 +5,8 @@ const Success = () => (
   <div className={s.body}>
     <div className={s.title}>Your account is being verified…</div>
     <div className={s.text}>
-      Your documents are successfully uploaded and being processed now.
-      This may take up to 15 minutes, please be patient and don’t try to
+      Your documents have been successfully uploaded and are being processed now.
+      This may take up to 30 minutes, please be patient and do not try to
       relaunch the verification process.
     </div>
   </div>
