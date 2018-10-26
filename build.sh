1243de39c5c94024e733bc9b075569c4a91329b7
aleserche <aleserche@gmail.com>
2018-10-26 16:15:19 +0500

    Adapted for a new project.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -ex
-DEV_IMAGE_NAME="jincort/mypizzapie-frontend-ico-dashboard-develop"
-PROD_IMAGE_NAME="jincort/mypizzapie-frontend-ico-dashboard"
+DEV_IMAGE_NAME="jincort/directcrypt-frontend-ico-dashboard-develop"
+PROD_IMAGE_NAME="jincort/directcrypt-frontend-ico-dashboard"
 TAG="${1}"
 
 docker push ${DEV_IMAGE_NAME}:${TAG}
